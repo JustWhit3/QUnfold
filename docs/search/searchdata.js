abab3ddff0c1f320a540a281bad498d6dcd79b35
Carl Timmer <timmer@jlab.org>
2023-06-15 21:25:03 +0000

    Update docs

--- docs/search/searchdata.js
+++ docs/search/searchdata.js
@@ -1,21 +1,24 @@
 var indexSectionsWithContent =
 {
-  0: "c",
+  0: "ceirw",
   1: "c",
-  2: "c"
+  2: "c",
+  3: "eirw"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "namespaces",
-  2: "files"
+  2: "files",
+  3: "functions"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Namespaces",
-  2: "Files"
+  2: "Files",
+  3: "Functions"
 };
 
