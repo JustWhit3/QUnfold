139dfd6d0ece8a22a380ceb6a4fb2c5759d73ab3
SimoneGasperini <simone.gasperini4@unibo.it>
2023-11-03 15:56:55 +0100

    Reset parameters values for a realistic unfolding analysis

--- studies/analysis.py
+++ studies/analysis.py
@@ -20,52 +20,54 @@ np.random.seed(seed)
 ##################################################################################
 distributions = {
     "normal": {
-        "parameters": (5.3, 1.4),  # (mu, sigma)
-        "lambda": 0.18,  # naivly optimized for 15 bins
+        "parameters": (5.3, 1.4)  # (mu, sigma)
     },
     "breit-wigner": {
-        "parameters": (5.3, 2.1),  # (mu, gamma)
-        "lambda": 0.039,  # naivly optimized for 15 bins
+        "parameters": (5.3, 2.1)  # (mu, gamma)
     },
     "exponential": {
-        "parameters": (2.5,),  # (tau)
-        "lambda": 0.0027,  # naivly optimized for 15 bins
+        "parameters": (2.5,)  # (tau)
     },
     "double-peaked": {
-        "parameters": (3.3, 0.9, 6.4, 1.2),  # ((mu1, sigma1), (mu2, sigma2))
-        "lambda": 0.022,  # naivly optimized for 15 bins
+        "parameters": (3.3, 0.9, 6.4, 1.2)  # ((mu1, sigma1), (mu2, sigma2))
     },
 }
 samples = 10000
-bins = 40
+bins = 20
 min_bin = 0.0
 max_bin = 10.0
-bias = -1.7
-smear = 0.5
+bias = -0.13
+smear = 0.21
 eff = 0.92
 ##################################################################################
 ##################################################################################
 
 
-def main():
+if __name__ == "__main__":
+
     for distr in distributions:
         true, meas, resp = generate_data(
             distr, samples, bins, min_bin, max_bin, bias, smear, eff
         )
 
-        unfolder = QUnfoldQUBO(resp, meas, lam=0.1)
+        unfolder = QUnfoldQUBO(resp, meas, lam=0.05)
+        energy_true = unfolder.compute_energy(x=true)
+
         t1 = time()
         unfolded_SA = unfolder.solve_simulated_annealing(num_reads=100, seed=seed)
         t2 = time()
         unfolded_HYB = unfolder.solve_hybrid_sampler()  # don't need any hyper-parameter
         t3 = time()
+        energy_SA = unfolder.compute_energy(x=unfolded_SA)
+        energy_HYB = unfolder.compute_energy(x=unfolded_HYB)
+
+        print("Energy true =", energy_true)
+        print("Energy SA =", energy_SA)
+        print("Energy HYB =", energy_HYB)
 
         binning = np.linspace(min_bin, max_bin, bins + 1)
         qunfold_plot_results(true, meas, unfolded_SA, distr, binning, solver="SA")
         print(f"Elapsed time for SA solver = {round(t2 - t1, 2)} seconds")
         qunfold_plot_results(true, meas, unfolded_HYB, distr, binning, solver="HYB")
         print(f"Elapsed time for HYB solver = {round(t3 - t2, 2)} seconds")
-
-
-if __name__ == "__main__":
-    main()
+        print()
