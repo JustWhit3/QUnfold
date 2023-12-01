59567af31cae88a42a8437665da23b19eb0ab098
Carl Timmer <timmer@jlab.org>
2023-12-01 11:20:59 +0000

    Update docs

--- docs/search/all_5.js
+++ docs/search/all_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['initialize_5fqubo_5fmodel_7',['initialize_qubo_model',['../classQUnfold_1_1core_1_1QUnfoldQUBO_1_1QUnfoldQUBO.html#af2157f107224d3a6b4295038bc30ff89',1,'QUnfold::core::QUnfoldQUBO::QUnfoldQUBO']]]
+  ['initialize_5fqubo_5fmodel_7',['initialize_qubo_model',['../classQUnfold_1_1core_1_1QUnfoldQUBO_1_1QUnfoldQUBO.html#ab93964acbf621e9d0afc381b472b5760',1,'QUnfold::core::QUnfoldQUBO::QUnfoldQUBO']]]
 ];
